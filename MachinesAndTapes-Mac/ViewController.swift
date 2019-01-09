391cf5b037ef26433c6d09229e7f9b40f360c918
Yamazaki Mitsuyoshi <mitsuyoshi.tsukuba@gmail.com>
2019-01-09 21:31:59 +0900

    Add targets

---
+++ MachinesAndTapes-Mac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MachinesAndTapes-Mac
+//
+//  Created by Yamazaki Mitsuyoshi on 2019/01/09.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
---
+++ MachinesAndTapes-iOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MachinesAndTapes-iOS
+//
+//  Created by Yamazaki Mitsuyoshi on 2019/01/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
